e505fdc6edca2ed942423a213e0223320e46641e
Bilal H <hussain.bilal000786@gmail.com>
2018-01-09 16:58:47 +0530

    Modified mongoose file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//const dburl = "mongodb://localhost:27017/TodoApp";
-const dburl = "mongodb://dbu_786:dbu@786@ds247357.mlab.com:47357/todo_database";
-
-mongoose.connect(dburl, {
+mongoose.connect('mongodb://ryanhssn:admin123@ds247357.mlab.com:47357/todo_database', {
 	useMongoClient: true
 })
 
