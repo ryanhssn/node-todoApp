5da13d6b0a7b4848e0dde2883a9e3f5093a4516a
Bilal H <hussain.bilal000786@gmail.com>
2018-01-16 17:19:40 +0530

    Add DELETE /users/me/token route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,10 +1,7 @@
 const {ObjectID} = require('mongodb');
-
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
-
-
 Todo.findByIdAndRemove('5a54b9f3ec1329413cdc0633').then((todo) => {
-	console.log(todo);
+console.log(todo);
 });
